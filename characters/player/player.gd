49d6da74cd2c47689637e065a16ddaea37b260be
WaffleBaby1 <austin.lingenfelter@gmail.com>
2024-01-18 10:15:46 -0600

    Update player.gd

--- characters/player/player.gd
+++ characters/player/player.gd
@@ -11,7 +11,7 @@ func _physics_process(delta):
 		direction += Vector2(0,-1)
 	if Input.is_action_pressed("down"):
 		direction += Vector2(0,1)
-  if Input.is_action_pressed("left"):
+	if Input.is_action_pressed("left"):
 		direction += Vector2(-1,0)
 	if Input.is_action_pressed("right"):
 		direction += Vector2(1,0)
